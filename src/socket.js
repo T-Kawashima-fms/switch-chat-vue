8baed6bc8b9a95d846a69e4bb9fdd7e124e1d488
takuya <ttttt.fms5@gmail.com>
2020-11-19 21:48:29 +0900

    firestore rules

--- src/socket.js
+++
@@ -1,9 +0,0 @@
-import VueSocketIO from 'vue-socket.io'
-
-const socket = new VueSocketIO({
-  debug: true,
-  connection: 'http://localhost:8080',
-  // options: { path: '/chatroom/:roomID' },
-})
-
-export default socket
