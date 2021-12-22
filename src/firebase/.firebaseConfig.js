b90e73a0d4d72bc9ea5bbb7448f725e3d4a28786
ttttt.fms5@gmail.com <ttttt.fms5@gmail.com>
2021-12-22 15:04:05 +0900

    recommit

--- src/firebase/.firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: process.env.VUE_APP_APIKEY,
-  authDomain: process.env.VUE_APP_AUTHDOMAIN,
-  databaseURL: process.env.VUE_APP_DATABASEURL,
-  projectId: process.env.VUE_APP_PROJECTID,
-  storageBucket: process.env.VUE_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
-  appId: process.env.VUE_APP_APPID
-};
